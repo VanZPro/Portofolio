1fa584b82a74a07c015c12cc2da1832b48979677
Evan Luis Tanjung <101246500+VanZPro@users.noreply.github.com>
2023-09-15 09:45:59 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,28 +1,29 @@
 <?php
+if (isset($_POST['uname']) && isset($_POST['password'])) {
 
-if (isset(_$POST['uname']) && isset (_$POST['password'])) {
+    function validate($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
 
-function validate($data){
-	$data = trim($data);
-	$data = stripslashes($data);
-	$data = htmlspecialchars($data);
-	return $data;
+    $username = validate($_POST['uname']);
+    $password = validate($_POST['password']);
+
+    if (empty($username)) {
+        header("Location: index.php?error=User Name is required");
+        exit();
+    }
+    if (empty($password)) {
+        header("Location: index.php?error=Password is required");
+        exit();
+    } else {
+        echo "Valid input";
+    }
+} else {
+    header("Location: index.php");
+    exit();
 }
-
-	$username = validate($_POST['uname']);
-	$password = validate($_POST['password']);
-
-	if (empty($uname)) {
-		header("Location : index.php?error=User Name is required");
-		exit();
-	}
-		if (empty($password)) {
-		header("Location : index.php?error=Password is required");
-		exit();
-	}else{
-		echo "Valid input";
-	}
-}else{
-	header("Location: index.php");
-	exit();
-}
+?>
