8d65dc37c18bfb158df58e752b2860dcd0a1c0a9
Evan Luis Tanjung <101246500+VanZPro@users.noreply.github.com>
2023-09-20 14:21:25 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+
+include "db_conn.php";
+
 if (isset($_POST['uname']) && isset($_POST['password'])) {
 
     function validate($data)
@@ -8,7 +12,6 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
         $data = htmlspecialchars($data);
         return $data;
     }
-
     $username = validate($_POST['uname']);
     $password = validate($_POST['password']);
 
@@ -20,8 +23,24 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
         header("Location: index.php?error=Password is required");
         exit();
     } else {
-        echo "Valid input";
+        $sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+
+        $result = mysqli_query($conn, $sql); 
+
+        if (mysqli_num_rows($ressult) === 1) {
+            $row = mysqli_fetch_assoc($ressult);
+            if ($row['user_name'] === $uname && $row['password'] === $pass) {
+                $SESSION['user_name']
+        }else{
+            header("Location: index.php?error=Incorrect Username or Password");
+            exit();
+            }
+        }else{
+            header("Location: index.php?error=Incorrect Username or Password");
+            exit();
+        }
     }
+
 } else {
     header("Location: index.php");
     exit();
